f1bedef1e1ef4dedfb834eafc3810d89f5aefea0
saoraw10-prog <saoraw10@gmail.com>
2026-09-11 22:18:07 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-
+const CACHE='anes-roster-v8';const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
